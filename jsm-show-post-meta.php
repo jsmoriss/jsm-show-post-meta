d7e72c5318720f309d6bf6289e78e072af74451e
JS Morisset <jsm@surniaulula.com>
2017-04-16 09:06:04 +0200

    v1.0.8

--- jsm-show-post-meta.php
+++ jsm-show-post-meta.php
@@ -32,7 +32,6 @@ if ( ! class_exists( 'JSM_Show_Post_Meta' ) ) {
 	class JSM_Show_Post_Meta {
 
 		private static $instance;
-		private static $wp_min_version = 3.7;
 	
 		public $view_cap;
 	
@@ -56,7 +55,9 @@ if ( ! class_exists( 'JSM_Show_Post_Meta' ) ) {
 
 		public static function check_wp_version() {
 			global $wp_version;
-			if ( version_compare( $wp_version, self::$wp_min_version, '<' ) ) {
+			$wp_min_version = 3.7;
+
+			if ( version_compare( $wp_version, $wp_min_version, '<' ) ) {
 				$plugin = plugin_basename( __FILE__ );
 				if ( is_plugin_active( $plugin ) ) {
 					self::load_textdomain();
@@ -67,7 +68,7 @@ if ( ! class_exists( 'JSM_Show_Post_Meta' ) ) {
 					deactivate_plugins( $plugin, true );	// $silent = true
 					wp_die( 
 						'<p>'.sprintf( __( '%1$s requires %2$s version %3$s or higher and has been deactivated.',
-							'jsm-show-post-meta' ), $plugin_data['Name'], 'WordPress',self::$wp_min_version ).'</p>'.
+							'jsm-show-post-meta' ), $plugin_data['Name'], 'WordPress', $wp_min_version ).'</p>'.
 						'<p>'.sprintf( __( 'Please upgrade %1$s before trying to re-activate the %2$s plugin.',
 							'jsm-show-post-meta' ), 'WordPress', $plugin_data['Name'] ).'</p>'
 					);
