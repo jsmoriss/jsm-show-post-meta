9e5c966b5b589694d7b774a79081e758387eb20f
JS Morisset <jsm@surniaulula.com>
2017-08-02 01:14:11 +0200

    v1.0.8

--- jsm-show-post-meta.php
+++ jsm-show-post-meta.php
@@ -11,7 +11,7 @@
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: Show all post meta (aka custom fields) keys and their unserialized values in a metabox on post editing pages.
  * Requires At Least: 3.7
- * Tested Up To: 4.8
+ * Tested Up To: 4.8.1
  * Version: 1.0.8
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
