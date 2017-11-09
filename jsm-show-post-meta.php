81749fdfe5b9ea259e6ab16d00fccca16e00e27e
JS Morisset <jsm@surniaulula.com>
2017-11-08 20:59:26 -0600

    v1.0.8

--- jsm-show-post-meta.php
+++ jsm-show-post-meta.php
@@ -12,7 +12,7 @@
  * Description: Show all post meta (aka custom fields) keys and their unserialized values in a metabox on post editing pages.
  * Requires PHP: 5.3
  * Requires At Least: 3.7
- * Tested Up To: 4.8.3
+ * Tested Up To: 4.9
  * Version: 1.0.8
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
