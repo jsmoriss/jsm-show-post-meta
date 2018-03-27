9f700c43ba8ecbba546f7b50373cf09ae1ac0ff2
JS Morisset <jsm@surniaulula.com>
2018-03-27 14:23:48 -0600

    v1.0.8

--- jsm-show-post-meta.php
+++ jsm-show-post-meta.php
@@ -34,13 +34,14 @@ if ( ! class_exists( 'JSM_Show_Post_Meta' ) ) {
 	class JSM_Show_Post_Meta {
 
 		private static $instance;
+		private static $wp_min_version = 3.8;
 	
 		public $view_cap;
 	
 		private function __construct() {
 			if ( is_admin() ) {
 				add_action( 'plugins_loaded', array( __CLASS__, 'load_textdomain' ) );
-				add_action( 'admin_init', array( __CLASS__, 'check_wp_version' ) );
+				add_action( 'admin_init', array( __CLASS__, 'check_wp_version' ) );	// Requires WP v3.8 or better.
 				add_action( 'add_meta_boxes', array( &$this, 'add_meta_boxes' ), 1000, 2 );
 			}
 		}
@@ -58,9 +59,7 @@ if ( ! class_exists( 'JSM_Show_Post_Meta' ) ) {
 
 		public static function check_wp_version() {
 			global $wp_version;
-			$wp_min_version = 3.8;
-
-			if ( version_compare( $wp_version, $wp_min_version, '<' ) ) {
+			if ( version_compare( $wp_version, self::$wp_min_version, '<' ) ) {
 				$plugin = plugin_basename( __FILE__ );
 				if ( is_plugin_active( $plugin ) ) {
 					if ( ! function_exists( 'deactivate_plugins' ) ) {
@@ -70,7 +69,7 @@ if ( ! class_exists( 'JSM_Show_Post_Meta' ) ) {
 					deactivate_plugins( $plugin, true ); // $silent = true
 					wp_die( 
 						'<p>' . sprintf( __( '%1$s requires %2$s version %3$s or higher and has been deactivated.',
-							'jsm-show-post-meta' ), $plugin_data['Name'], 'WordPress', $wp_min_version ) . '</p>' . 
+							'jsm-show-post-meta' ), $plugin_data['Name'], 'WordPress', self::$wp_min_version ) . '</p>' . 
 						'<p>' . sprintf( __( 'Please upgrade %1$s before trying to re-activate the %2$s plugin.',
 							'jsm-show-post-meta' ), 'WordPress', $plugin_data['Name'] ) . '</p>'
 					);
