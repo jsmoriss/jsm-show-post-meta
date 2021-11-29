738caaaef15b1afd87a632d9cc9116577ab4008f
JS Morisset <jsm@surniaulula.com>
2021-11-29 14:44:02 -0800

    v2.0.0

--- js/jquery-block-editor.js
+++ js/jquery-block-editor.js
@@ -7,16 +7,13 @@ var jsmspmBlockEditor = ( function(){
 	var adminPageL10n     = 'jsmspmAdminPageL10n';
 
 	return {
-		refreshPostbox: function(){						// Called by wp.data.subscribe().
+		refreshPostbox: function(){					// Called by wp.data.subscribe().
 
-			var isSavingMb = isSavingMetaBoxes();				// Check if we're saving metaboxes.
+			var isSavingMb = isSavingMetaBoxes();			// Check if we're saving metaboxes.
 
-			if ( wasSavingMb ) {						// Last check was saving metaboxes.
+			if ( wasSavingMb && ! isSavingMb ) {			// Check if done saving metaboxes.
 
-				if ( ! isSavingMb ) {					// Saving metaboxes is done.
-
-					sucomBlockPostbox( pluginId, adminPageL10n );	// Refresh our metabox(es).
-				}
+				sucomBlockPostbox( pluginId, adminPageL10n );	// Refresh our metabox(es).
 			}
 
 			wasSavingMb = isSavingMb;
@@ -25,3 +22,4 @@ var jsmspmBlockEditor = ( function(){
 })();
 
 wp.data.subscribe( jsmspmBlockEditor.refreshPostbox );
+
