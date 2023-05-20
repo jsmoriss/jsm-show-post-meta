e9b58b078b3e9a8801a9ba6ae35d2699aff3e23c
JS Morisset <jsm@surniaulula.com>
2023-05-20 10:07:46 -0700

    v3.0.12

--- jsm-show-post-meta.php
+++ jsm-show-post-meta.php
@@ -12,7 +12,7 @@
  * Description: Show post metadata (aka custom fields) in a metabox when editing posts / pages - a great tool for debugging issues with post metadata.
  * Requires PHP: 7.2.5
  * Requires At Least: 5.5
- * Tested Up To: 6.2.1
+ * Tested Up To: 6.2.2
  * Version: 3.0.12
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
