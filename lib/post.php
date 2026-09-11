5861abb287f970528449a3f03a86dcbfb9b96676
jsmoriss <jsm@surniaulula.com>
2026-09-11 16:38:14 +0000

    v4.9.1-dev.1

--- lib/post.php
+++ lib/post.php
@@ -151,8 +151,8 @@ if ( ! class_exists( 'JsmSpmPost' ) ) {
 			 * so that jQuery can hide the table row after a successful delete.
 			 */
 			$metabox_id   = 'jsmspm';
-			$obj_id       = SucomUtil::sanitize_int( $_POST[ 'obj_id' ] );	// Returns integer or null.
-			$meta_key     = SucomUtil::sanitize_meta_key( $_POST[ 'meta_key' ] );
+			$obj_id       = SucomUtil::sanitize_int( $_POST[ 'obj_id' ] );		// Returns integer or null.
+			$meta_key     = SucomUtil::sanitize_meta_key( $_POST[ 'meta_key' ] );	// Decode/remove html/js/css and quotes.
 			$table_row_id = SucomUtil::sanitize_key( $metabox_id . '_' . $obj_id . '_' . $meta_key );
 			$post_obj     = get_post( $obj_id );
 			$delete_cap   = apply_filters( 'jsmspm_delete_meta_capability', 'manage_options', $post_obj );
