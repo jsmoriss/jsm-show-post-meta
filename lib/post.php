570341d44c8d4b0bc87456f6d24d62a5fddc43d7
jsmoriss <jsm@surniaulula.com>
2026-09-12 14:35:21 +0000

    v4.9.1-rc.1

--- lib/post.php
+++ lib/post.php
@@ -152,7 +152,7 @@ if ( ! class_exists( 'JsmSpmPost' ) ) {
 			 */
 			$metabox_id   = 'jsmspm';
 			$obj_id       = SucomUtil::sanitize_int( $_POST[ 'obj_id' ] );		// Returns integer or null.
-			$meta_key     = SucomUtil::sanitize_meta_key( $_POST[ 'meta_key' ] );	// Decode/remove html/js/css and quotes.
+			$meta_key     = SucomUtil::sanitize_meta_key( $_POST[ 'meta_key' ] );	// Decode and remove html/js/css and quotes.
 			$table_row_id = SucomUtil::sanitize_key( $metabox_id . '_' . $obj_id . '_' . $meta_key );
 			$post_obj     = get_post( $obj_id );
 			$delete_cap   = apply_filters( 'jsmspm_delete_meta_capability', 'manage_options', $post_obj );
